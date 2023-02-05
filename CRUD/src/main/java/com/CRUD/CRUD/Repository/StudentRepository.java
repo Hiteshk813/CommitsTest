7b75439b9aa57ffe5fa0e46d8206ade3d2a8f316
Hitesh Anney <Hiteshk813@gmail.com>
2023-02-05 13:18:12 +0530

    Commit

--- CRUD/src/main/java/com/CRUD/CRUD/Repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.CRUD.CRUD.Repository;
-
-import com.CRUD.CRUD.Model.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository  extends CrudRepository<Student, Integer> {
-}
